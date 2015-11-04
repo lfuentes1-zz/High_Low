81b8bd4d56c067e7dc964259f6e0533044b81520
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-11-04 11:29:03 -0600

    High_Low Game - Uses rand PHP fucntion

---
+++ highlow.php
@@ -0,0 +1,26 @@
+<?php
+
+	define ("MIN", "1");
+	define ("MAX", "100"); 
+
+	$random_number = rand (MIN, MAX);	
+
+	do {
+		fwrite(STDOUT, "Can you guess my number? ");
+
+		//Typecasting $users_guess to an int since fgets returns a string
+		$users_guess = (int)trim(fgets(STDIN));
+		// var_dump($users_guess);
+
+		if ($users_guess < $random_number) {
+			fwrite(STDOUT, "You guessed too low, guess higher! " . PHP_EOL);
+		} elseif ($users_guess > $random_number) {
+			fwrite(STDOUT, "You guessed too high, guess lower!\n");		
+		} else {
+			fwrite(STDOUT, "Right now, you guessed correctly!\n");
+		}
+	}		
+	while ($random_number != $users_guess);
+
+	exit (0);
+?>
