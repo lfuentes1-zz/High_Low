3b8375b90090eb2d027da8871a193a8ed0643da3
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-11-04 14:51:39 -0600

    High_Low Game - Added Arguments

--- highlow.php
+++ highlow.php
@@ -1,18 +1,36 @@
 <?php
 
-	define ("MIN", "1");
-	define ("MAX", "100"); 
+	if ($argc != 3) {
+		die ("Incorrect number of arguments passed.  Pass a min and a max" . PHP_EOL);
+	}
+	if ($argc == 3) {
 
-	// $random_number = rand (MIN, MAX);	
-	//mt_rand generates a better random value by using an algorithm that allows random numbers to be generated much faster
-	$random_number = mt_rand (MIN, MAX);
+		$filename = $argv[0];
+		$min = $argv[1];
+		$max = $argv[2];
+
+		if (!((is_numeric($min)) && (is_numeric($max)))) {
+			die ("Arguments to $filename must be numeric representing min and max." . PHP_EOL);
+		}
+		
+		//mt_rand generates a better random value by using an algorithm that allows random numbers to be generated much faster
+		if ($min > $max)
+		{
+			$random_number = mt_rand ($max, $min);
+			echo "Guess a number between {$max} and {$min}." . PHP_EOL;
+
+		} else {
+			$random_number = mt_rand ($min, $max);
+			echo "Guess a number between {$min} and {$max}." . PHP_EOL;
+		}
+	}
+	
 	$try_number = 1;
-
 	do {
 		fwrite(STDOUT, "Can you guess my number? ");
 
 		$users_guess = trim(fgets(STDIN));
-		var_dump($users_guess);
+		// var_dump($users_guess);
 		
 		//is_numeric is a PHP function that checks if the $users_guess is a numeric value or a numeric string value
 		//ctype-digit is also another function PHP that can provide the same desired results
